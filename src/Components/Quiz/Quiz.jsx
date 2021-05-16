c924fddd6cef457cba17ee1d612b1aee03209592
internetcharles <charlesodellsmith@gmail.com>
2021-05-16 13:08:30 -0700

    individual quiz page

--- src/Components/Quiz/Quiz.jsx
+++ src/Components/Quiz/Quiz.jsx
@@ -8,9 +8,43 @@ export const Quiz = () => {
   const { loading, quiz } = useQuiz(id);
 
   if (loading) return <h1>Loading...</h1>;
+  console.log(quiz);
+
+  const parsedQuestions = JSON.parse(quiz.questions);
+  const parsedCorrectAnswers = JSON.parse(quiz.correctAnswers);
+  const parsedIncorrectAnswer1 = JSON.parse(quiz.incorrectAnswer1);
+  const parsedIncorrectAnswer2 = JSON.parse(quiz.incorrectAnswer2);
+  const parsedIncorrectAnswer3 = JSON.parse(quiz.incorrectAnswer3);
+
   return (
     <div>
-      {console.log(quiz)}
+      {console.log(parsedQuestions)}
+      <h2>{quiz.title}</h2>
+      <h3>{quiz.description}</h3>
+      {parsedQuestions.map((question, idx) => (
+        <>
+          <div>{question}</div>
+          <div>
+            Correct Answer:
+            {' '}
+            <br />
+            {parsedCorrectAnswers[idx]}
+          </div>
+          <div>
+            Incorrect Answers:
+            {' '}
+            <br />
+            {parsedIncorrectAnswer1[idx]}
+            {' '}
+            {' - '}
+            {' '}
+            {parsedIncorrectAnswer2[idx]}
+            {' - '}
+            {' '}
+            {parsedIncorrectAnswer3[idx]}
+          </div>
+        </>
+      ))}
     </div>
   );
 };
